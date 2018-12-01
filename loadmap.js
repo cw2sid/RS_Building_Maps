7f96a3d6b5ced654ab699aada5e2b67fb46dfebc
cw2sid <annalisa.wilde.test@gmail.com>
2018-11-30 23:58:46 -0500

    being nice

--- loadmap.js
+++ loadmap.js
@@ -1,28 +1,34 @@
 function loadmap(districts) {
+  //define what we have
   var width = 1400,
   height = 1400
   scale=80000;
+  //define zoom
   const zoom = d3.zoom()
   .scaleExtent([1, 20])
   .translateExtent([[0,0], [width, height]])
   .extent([[0, 0], [width+200, height]])
   .on("zoom", zoomed);
+  //define projection
+  var svg = d3.select('svg')
+  .call(zoom);
   var albersProjection = d3.geoAlbers()
   .scale(scale)
   .rotate([73.6533, 0])
   .center([0, 40.4806])
   .translate([width/2, height/2]);
-  
-  var z = d3.scaleOrdinal()
-      .range(["#dc2f0d","#12049d"]);
-  var svg = d3.select('svg')
-  .call(zoom);
-  var legendlable=["Lost Units","Gained Units"];
   var geoPath = d3.geoPath()
   .projection(albersProjection);
+  // need to clear
   d3.selectAll("g").remove();
+  //then append
   var g = svg.append('g')
   var buildinglayer = svg.append('g')
+  //just for legend
+  var z = d3.scaleOrdinal()
+      .range(["#dc2f0d","#12049d"]);
+  //make legend
+  var legendlable=["Lost Units","Gained Units"];
   var legend = svg.append('g')
       .attr("font-family", "sans-serif")
       .attr("font-size", 10)
@@ -30,47 +36,51 @@ function loadmap(districts) {
     .selectAll("g")
     .data(legendlable)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * 40 + ")"; });
-      legend.append("circle")
-          .attr("r", 10)
-          .attr("cx",100)
-          .attr("cy",50)
-          .attr('fill-opacity','.75')
-          .attr("fill", z);
-    
-      legend.append("text")
-          .attr("x", 80)
-          .attr("y", 50)
-          .attr("dy", "0.32em")
-          .text(function(d) { return d; });
-
+    .attr("transform", function(d, i) { return "translate(0," + i * 40 + ")"; });
+  legend.append("circle")
+    .attr("r", 10)
+    .attr("cx",100)
+    .attr("cy",50)
+    .attr('fill-opacity','.75')
+    .attr("fill", z);  
+  legend.append("text")
+    .attr("x", 80)
+    .attr("y", 50)
+    .attr("dy", "0.32em")
+    .text(function(d) { return d; });
+  //make tooltip
   var div = d3.select("body").append("div")	
   .attr("class", "tooltip")				
   .style("opacity", 0);
-  
+  //set defualt district boundaries
   var geojsonurl="https://data.cityofnewyork.us/resource/jp9i-3b7y.geojson"
   if (districts !=""){
     geojsonurl = districts
   }
+  //send request for geojson data
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var community_districts = JSON.parse(this.responseText);
+    if (this.readyState == 4 && this.status == 200) { //got geojson response
+      //parse the response
+      var districtBoundaries = JSON.parse(this.responseText);
+      //add the paths to the svg
       g.selectAll('path')
-      .data(community_districts.features)
+      .data(districtBoundaries.features)
       .enter()
       .append('path')
       .attr('fill', '#ccc')
       .attr('class','lines')
       .attr('d', geoPath);
-      
+      //load building data after load 
       loadbuildings()
     }
   };
+  //actual call to endpoint
   xmlhttp.open("GET",geojsonurl, true);
   xmlhttp.send(); 
-  
+  //to load the buildings
   function loadbuildings(){
+    //parse the data from the csv, then
     d3.csv("changes-summary_2007-2014.csv",function(d){
       return{
         address: d.address,
@@ -86,7 +96,7 @@ function loadmap(districts) {
         drie: d.drie,
         c420c: d["420c"]
       }
-    },function(buildings){
+    },function(buildings){ // for each data element add circle
       buildinglayer.selectAll('circle')
       .data(buildings)
       .enter()
@@ -99,9 +109,9 @@ function loadmap(districts) {
       .attr("cy",function(d){
             return albersProjection([d.longitude,d.latitude])[1];})
       .attr("r",function(d){
-              return .5+Math.abs(d.diff/500)
+              return d3.event?.5+Math.abs(d.diff/500)/d3.event.transform.k:.5+Math.abs(d.diff/500)
       })
-      .on("mouseover", function(d) {
+      .on("mouseover", function(d) { //defines tooltip behaviour
               div.transition()		
               .duration(200)		
               .style("opacity", .9);		
@@ -122,8 +132,7 @@ function loadmap(districts) {
           }
         );
       }
-      
-      
+      // color selection
       function color(change){
         if (change > 0) {
           return "#12049d"
@@ -133,9 +142,11 @@ function loadmap(districts) {
           return "#dc2f0d"
         }
       }
-    function zoomed() {
+      //zoom supporter function - applies transform
+      function zoomed() {
         g.attr("transform", d3.event.transform);
-        buildinglayer.attr("transform", d3.event.transform);
+        buildinglayer.attr("attr",d3.event.transform);
+        
       };
       
       
